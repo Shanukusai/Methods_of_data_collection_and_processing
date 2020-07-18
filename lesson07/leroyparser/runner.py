1559c4cd0309a3f957e134d196ea59ade48551df
Shanukusai <44548063+Shanukusai@users.noreply.github.com>
2020-07-18 15:14:13 +0300

    Add files via upload

---
+++ lesson07/leroyparser/runner.py
@@ -0,0 +1,16 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from leroyparser.spiders.leroymerlinru import LeroymerlinruSpider
+from leroyparser import settings
+
+if __name__ == '__main__':
+    search = 'картины на стекле'
+
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+
+    process.crawl(LeroymerlinruSpider, search=search)
+
+    process.start()
